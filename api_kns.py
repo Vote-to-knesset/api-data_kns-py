87a1b21e6d397f4774c622029e7efa2ad404fbde
eli barak <barak3656@gmail.com>
2023-11-21 14:33:41 +0200

    update sort

--- api_kns.py
+++ api_kns.py
@@ -132,7 +132,8 @@ def get_data_parties_from_db():
 @app.route('/api/data_bills', methods=['GET'])
 def api_data():
     data = get_data_bills_from_db()
-    sorted_data = sort_bills_by_interest(data).reverse()
+    sorted_data = sort_bills_by_interest(data)
+    sorted_data.reverse()
     response = json.dumps(sorted_data, ensure_ascii=False).encode('utf8')
     return response
 
